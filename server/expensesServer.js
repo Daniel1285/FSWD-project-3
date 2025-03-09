5f12388a524a7b6deda18a5a7353135d2bf85789
Yoni Klein <yonatankleinse@gmail.com>
2025-03-09 15:05:16 +0200

    ithink end server

--- server/expensesServer.js
+++ server/expensesServer.js
@@ -1,22 +1,70 @@
 import database from "./databases/database.js";
+class ExpensesServer {
+    constructor() {
+        this.database = database;
+    }
 
-function getUserExpenses() {
-    return database.expenses.getExpenses(database.currentUser.getCurrentUser().name);
+    getUserExpenses() {
+        return this.database.expenses.getExpenses(this.database.currentUser.getCurrentUser().name);
+    }
+
+    getExpense(id) {
+        return this.getUserExpenses().find((e) => e.id === id);
+    }
+
+    postExpense(expense) {
+        return this.database.expenses.postExpense(this.database.currentUser.getCurrentUser().name, expense);
+    }
+
+    putExpense(id, expense) {
+        return this.database.expenses.putExpense(this.database.currentUser.getCurrentUser().name, id, expense);
+    }
+
+    deleteExpense(id) {
+        return this.database.expenses.deleteExpense(this.database.currentUser.getCurrentUser().name, id);
+    }
+
+    request(request, callback) {
+        let response = { success: true, body: "" };
+
+        switch (request.method) {
+            case "GET":
+                if (request.url.endsWith("/expenses")) {
+                    response.body = this.getUserExpenses();
+                } else if (request.url.match(/\/expenses\/\d+$/)) {
+                    let id = parseInt(request.url.split("/").pop());
+                    response.body = this.getExpense(id);
+                }
+                break;
+            case "POST":
+                if (request.url.endsWith("/expenses")) {
+                    let expense = JSON.parse(request.data);
+                    response.body = this.postExpense(expense);
+                }
+                break;
+            case "PUT":
+                if (request.url.match(/\/expenses\/\d+$/)) {
+                    let id = parseInt(request.url.split("/").pop());
+                    let expense = JSON.parse(request.data);
+                    response.body = this.putExpense(id, expense);
+                }
+                break;
+            case "DELETE":
+                if (request.url.match(/\/expenses\/\d+$/)) {
+                    let id = parseInt(request.url.split("/").pop());
+                    response.body = this.deleteExpense(id);
+                }
+                break;
+            default:
+                response.success = false;
+                response.body = "Invalid request method";
+        }
+
+        request.responseText = JSON.stringify(response);
+        callback();
+    }
 }
 
-function getExpense(id) {
-    return getUserExpenses().find((e) => e.id === id);
-}
+const expensesServer = new ExpensesServer(database);
 
-function postExpense(expense) {
-    return database.expenses.postExpense(database.currentUser.getCurrentUser().name, expense);
-}
-
-function putExpense(id, expense) {
-    return database.expenses.putExpense(database.currentUser.getCurrentUser().name, id, expense);
-}
-
-function deleteExpense(id) {
-    return database.expenses.deleteExpense(database.currentUser.getCurrentUser().name, id);
-}
-
+export { expensesServer };
--- server/usersServer.js
+++ server/usersServer.js
@@ -1,32 +1,83 @@
 import database from "./databases/database.js";
 
-function createUser(user) {
-    if (database.users.getUser(user.name)) {
-        return false;
+class UsersServer {
+    constructor() {
+        this.database = database;
     }
-    return database.users.postUser(user);
+
+    createUser(user) {
+        if (this.database.users.getUser(user.name)) {
+            return false;
+        }
+        return this.database.users.postUser(user);
+    }
+
+    connectUser(name, password) {
+        let user = this.database.users.getUser(name);
+        if (!user) {
+            return false;
+        }
+        if (user.password !== password) {
+            return false;
+        }
+        this.database.currentUser.postCurrentUser(user);
+    }
+
+    disconnectUser() {
+        this.database.currentUser.logoutUser();
+    }
+
+    changePassword(name, password) {
+        let user = this.database.users.getUser(name);
+        if (!user) {
+            return false;
+        }
+        user.password = password;
+        return this.database.users.putUser(user, name);
+    }
+
+    deleteUser(name) {
+        return this.database.users.deleteUser(name);
+    }
+
+    request(request, callback) {
+        let response = { success: true, body: "" };
+
+        switch (request.method) {
+            case "POST":
+                if (request.url.endsWith("/create")) {
+                    let user = JSON.parse(request.data);
+                    response.body = this.createUser(user);
+                } else if (request.url.endsWith("/connect")) {
+                    let { name, password } = JSON.parse(request.data);
+                    response.body = this.connectUser(name, password);
+                } else if (request.url.endsWith("/disconnect")) {
+                    this.disconnectUser();
+                    response.body = true;
+                }
+                break;
+            case "PUT":
+                if (request.url.endsWith("/changePassword")) {
+                    let { name, password } = JSON.parse(request.data);
+                    response.body = this.changePassword(name, password);
+                }
+                break;
+            case "DELETE":
+                if (request.url.endsWith("/delete")) {
+                    let { name } = JSON.parse(request.data);
+                    response.body = this.deleteUser(name);
+                }
+                break;
+            default:
+                response.success = false;
+                response.body = "Invalid request method";
+        }
+
+        request.responseText = JSON.stringify(response);
+        callback();
+    }
 }
 
-function connectUser(name, password) {
-    let user = database.users.getUser(name);
-    if (!user) {
-        return false;
-    }
-    if (user.password !== password) {
-        return false;
-    }
-    database.currentUser.postCurrentUser(וser);
-}
+const usersServer = new UsersServer(database);
 
-function disconnectUser() {
-    database.currentUser.logoutUser();
-}
-
-function changePassword(name, password) {
-    let user = database.users.getUser(name);
-    if (!user) {
-        return false;
-    }
-    user.password = password;
-    return database.users.putUser(user, name);
-}
+export { usersServer };
